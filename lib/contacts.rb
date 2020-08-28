775a8567f37002d3569d34c95b153a201b4696d0
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-28 16:47:40 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,7 @@ require 'pry'
 
 def remove_strawberry(contacts)
   if person == "Freddy Mercury"
-    contact_details_hash.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
       binding.pry
     end
   end
